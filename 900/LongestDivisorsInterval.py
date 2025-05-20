d2495bd492e382e92ac3f0f29f4c0bcffb36b314
Padmnabh Tewari <91028496+PadmnabhTewari@users.noreply.github.com>
2025-05-21 02:05:32 +0530

    Update LongestDivisorsInterval.py

--- 900/LongestDivisorsInterval.py
+++ 900/LongestDivisorsInterval.py
@@ -1,1 +1,11 @@
-
+for i in range(int(input())):
+    a=int(input())
+    c=0
+    for j in range(1,a+1):
+        if(a%j!=0):
+            c=j
+            break
+    if(c==0):
+        print(a)
+    else:
+        print(c-1)
